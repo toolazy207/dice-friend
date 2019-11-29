24595d66b0c4cab2c94b9a668243bc823301d2ae
toolazy207 <52755820+toolazy207@users.noreply.github.com>
2019-11-29 10:53:27 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import discord
 from test import dicebot
+from test import dicemax
 
 client = discord.Client()
+token = "NjQ5Njg5MTM1MTc4ODQyMTUx.XeBg5w.dNdVOWvx_ikM92gHxhNBGfM-Ag0"
 
 
 @client.event
@@ -17,13 +19,19 @@ async def on_message(message):
     if message.content.startswith("$hello"):
         await message.channel.send("Hello!")
 
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startswith("!"):
-        newMessage = message.content.split("!")
+    if message.content.startswith("/") and "k" not in message.content:
+        newMessage = message.content.split("/")
         diceMessage = dicebot(newMessage[-1])
         await message.channel.send(diceMessage)
+    if "/" and "d" and "k" in message.content and message.content.startswith("/"):
+        newMessage = message.content.split("/")
+        diceMessage = dicemax(newMessage[-1])
+        await message.channel.send(diceMessage)
 
-client.run("NjQ5Njg5MTM1MTc4ODQyMTUx.XeAcSw.1Bknlnw1VWoGVyjvXAC63mK-c84")
+
+client.run(token)
--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import random
 
-
 def dicebot(message):
     mess = message.split("d")
     mod = 0
@@ -13,10 +12,10 @@ def dicebot(message):
                 negString = mess[1]
                 modIncrement += 1
                 negStringSplit = negString.split("-")
+                typeOfDie = int(negStringSplit[0])
                 negStringSplit[0] = "0"
                 for num in negStringSplit:
                     mod += (int(num) * (-1))
-                typeOfDie = int(mess[0])
                 break
             break
 
@@ -25,10 +24,10 @@ def dicebot(message):
                 posString = mess[1]
                 modIncrement += 1
                 posStringSplit = posString.split("+")
+                typeOfDie = int(posStringSplit[0])
                 posStringSplit[0] = "0"
                 for num in posStringSplit:
                     mod += int(num)
-                typeOfDie = int(mess[0])
                 break
             break
         else:
@@ -62,8 +61,84 @@ def dicebot(message):
     return modStr, diceRollStr, resultString
 
 
+def dicemax(message):
+    mess = message.split("d")
+    maxmess = mess[1].split("k")
+    dieType = maxmess[0]
+    modIncrement = 0
+    mod = 0
+    diceRollStr = "Rolls: "
+    maxRollStr = "Max Rolls: "
+    maxList = []
+    resultStr = "Results: "
+    if mess[0] == "":
+        timesDice = 1
+    else:
+        timesDice = int(mess[0])
+    while "+" or "-" in maxmess[1]:
+        if "+" in maxmess[1]:
+            while modIncrement == 0:
+                maxmessPlus = maxmess[-1].split("+")
+                maxmessMod = int(maxmessPlus[0])
+                maxmessPlus[0] = "0"
+                for num in maxmessPlus:
+                    mod += int(num)
+                modIncrement += 1
+                break
+            break
 
+        elif "-" in maxmess[1]:
+            while modIncrement == 0:
+                maxmessMinus = maxmess[-1].split("-")
+                maxmessMod = int(maxmessMinus[0])
+                maxmessMinus[0] = "0"
+                for num in maxmessMinus:
+                    mod -= int(num)
+                modIncrement += 1
+                break
+            break
 
+        else:
+            maxmessMod = int(maxmess[-1])
+            break
 
+    modString = ("Mod: " + str(mod))
+
+    while timesDice != 0:
+        diceRoll = random.randint(1, int(dieType))
+
+        if timesDice != 1:
+            diceRoll = random.randint(1, int(dieType))
+            maxList.append(str(diceRoll))
+            diceRollStr += (str(diceRoll) + ", ")
+        else:
+            maxList.append(str(diceRoll))
+            diceRollStr += str(diceRoll)
+        timesDice -= 1
+    print(maxList)
+    maxRollList = []
+    while maxmessMod != 0:
+        maxRoll = max(maxList, key=lambda x: int(x))
+        if maxmessMod == 1:
+            maxRollStr += str(maxRoll)
+            maxRollList.append(str(maxRoll))
+        else:
+            maxRollStr += (str(maxRoll) + ", ")
+            maxRollList.append(str(maxRoll))
+        maxList.remove(max(maxList, key=lambda x: int(x)))
+        maxmessMod -= 1
+    if mod != 0:
+        print(maxRollList)
+        for number in maxRollList:
+            resultStr += (str(int(number) + mod) + ", ")
+
+    else:
+        pass
+    if resultStr.endswith(", "):
+        resultStr = resultStr[:-2]
+    if mod == 0:
+        return modString, diceRollStr, maxRollStr
+    else:
+        return modString, diceRollStr, maxRollStr, resultStr
 
 
